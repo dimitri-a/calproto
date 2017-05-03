6321f45a45c966ab1cca41c003cc7665f99b7330
Dimitri Couwenberg <dimitri.couwenberg@team.telstra.com>
2017-05-03 18:42:10 +1000

    first calendar

---
+++ client/components/calendar.js
@@ -0,0 +1,13 @@
+<div>
+    yoho moi je suis the calendar
+
+    <div className="row seven-cols">
+        <div className="bord col-md-1 midd">SUN <br/> 12 </div>
+        <div className="bord  col-md-1 midd">MON <br/> 13</div>
+        <div className="bord col-md-1 midd">TUE<br/> 14</div>
+        <div className="bord col-md-1 midd">WED<br/> 15</div>
+        <div className="bord col-md-1 midd">THU<br/> 16</div>
+        <div className="bord col-md-1 midd">FRI<br/> 17</div>
+        <div className="bord col-md-1 midd">SAT<br/> 18</div>
+    </div>
+</div>

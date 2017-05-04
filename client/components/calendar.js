3b4e4a40c4dbc6132140f27b72b519e29456d88d
Dimitri Couwenberg <dimitri.couwenberg@team.telstra.com>
2017-05-04 14:03:17 +1000

    setstate works

--- client/components/calendar.js
+++ client/components/calendar.js
@@ -1,8 +1,32 @@
 import React, {Component} from 'react';
 import Day from './day';
+let today;
 
 export default class Calendar extends Component {
 
+
+    constructor(props) {
+        super();
+        this.currentWeek();
+        this.state = {
+            today: {
+                "date": "1/1/2017",
+                "morning": {
+                    "text": "AM",
+                    "period": "7am-10am"
+                },
+                "arvo": {
+                    "text": "AM/PM",
+                    "period": "11am-2pm"
+                },
+
+                "evening": {
+                    "text": "PM",
+                    "period": "2pm-4pm"
+                }
+            }
+        };
+    }
 
     nextWeek() {
         //get timeslots/details next week
@@ -16,53 +40,51 @@ export default class Calendar extends Component {
 
         //todo remove
         console.log('prevweek');
+
+        this.setState({today:
+            {
+            "date": "3/2/2017",
+
+            "morning": {
+                "text": "AM",
+                "period": "6am-8am"
+            },
+            "arvo": {
+                "text": "AM/PM",
+                "period": "11am-2pm"
+            },
+
+            "evening": {
+                "text": "PM",
+                "period": "2pm-4pm"
+            }
+        }});
+
+        //todo remove
+        console.log('testing=prevweek', this.state.today);
     }
 
     currentWeek() {
-        //get currentweek
+        //todo remove
+        console.log('testing=currentweek');
 
     }
 
-
-
     render() {
-
-        const today=
-            {
-                "date": "1/1/2017",
-                "morning":
-                    {
-                        "text":"AM",
-                        "period": "7am-10am"
-                    },
-                "arvo":
-                    {
-                        "text":"AM/PM",
-                        "period": "11am-2pm"
-                    },
-
-                "evening":
-                    {
-                        "text":"PM",
-                        "period": "2pm-4pm"
-                    }
-            }
-
         return (
             <div>
-                <div className="row seven-cols">
-                    <div className="bord col-md-1 midd"> <Day day={today}/> </div>
-                    <div className="bord col-md-1 midd"> <Day day={today}/> </div>
-                    <div className="bord col-md-1 midd"> <Day day={today}/> </div>
-                    <div className="bord col-md-1 midd"> <Day day={today}/> </div>
-                    <div className="bord col-md-1 midd"> <Day day={today}/> </div>
-                    <div className="bord col-md-1 midd"> <Day day={today}/> </div>
-                    <div className="bord col-md-1 midd"> <Day day={today}/> </div>
-                </div>
                 <button className="btn" onClick={this.prevWeek.bind(this)}>prev week</button>
-
                 <button className="btn" onClick={this.nextWeek.bind(this)}>next week</button>
+                <div className="row seven-cols">
+                    <div className="bord col-md-1 midd"><Day day={this.state.today}/></div>
+                    <div className="bord col-md-1 midd"><Day day={this.state.today}/></div>
+                    <div className="bord col-md-1 midd"><Day day={this.state.today}/></div>
+                    <div className="bord col-md-1 midd"><Day day={this.state.today}/></div>
+                    <div className="bord col-md-1 midd"><Day day={this.state.today}/></div>
+                    <div className="bord col-md-1 midd"><Day day={this.state.today}/></div>
+                    <div className="bord col-md-1 midd"><Day day={this.state.today}/></div>
 
+                </div>
             </div>
         );
     }
